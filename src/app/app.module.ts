a70239962f8b096a92e439a16d171e639ac8ee7c
Héctor <hector.martos@nfq.es>
2018-10-13 23:23:42 +0200

    Removed tableau component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TableauComponent } from './tableau/tableau.component';
 import { TableauModule } from 'tableau';
 import { ScriptService } from './scripts.service';
 
 @NgModule({
-  declarations: [AppComponent, TableauComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, TableauModule],
   providers: [ScriptService],
   bootstrap: [AppComponent]
