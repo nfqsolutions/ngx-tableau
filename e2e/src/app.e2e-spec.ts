1f329ef81bd0edebf2e666c7f156811b72ea1b19
Héctor <hector.martos@nfq.es>
2019-04-29 09:24:32 +0200

    Fixed some tests and added ci npm script

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('ngx-tableau', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display tableauViz container', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ngx-tableau!');
+    expect(page.getTableauVizContainer()).toBeDefined();
   });
 });
