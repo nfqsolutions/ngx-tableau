8ac40f7ad7e261bf3ad7633a1a104e0a940f10ca
olgajuan <olga.juan@nfq.es>
2019-08-28 10:26:55 +0200

    Delete commented code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,19 +24,4 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     expect(app).toBeTruthy();
   }));
-
-  // it(`should have as title 'ngx-tableau'`, async(() => {
-  //   // const fixture = TestBed.createComponent(AppComponent);
-  //   // const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('ngx-tableau');
-  // }));
-
-  // it('should render title in a h1 tag', async(() => {
-  //   // const fixture = TestBed.createComponent(AppComponent);
-  //   // fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain(
-  //     'Welcome to ngx-tableau!'
-  //   );
-  // }));
 });
